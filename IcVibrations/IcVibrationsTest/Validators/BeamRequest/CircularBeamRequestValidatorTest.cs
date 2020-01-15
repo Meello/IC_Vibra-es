74e2bdcdff0f3b7b43e562bec466be3f72b02af9
Bruno Mello Silveira <bruno.silveira@stone.com.br>
2020-01-15 08:31:04 -0300

    Adding test to CircularBeamRequestValidator

--- IcVibrations/IcVibrationsTest/Validators/BeamRequest/CircularBeamRequestValidatorTest.cs
+++ IcVibrations/IcVibrationsTest/Validators/BeamRequest/CircularBeamRequestValidatorTest.cs
@@ -1,40 +1,38 @@
-﻿using IcVibrations.Core.Validators.BeamRequest;
+﻿using FluentAssertions;
+using IcVibrations.Core.Validators.BeamRequest;
 using IcVibrations.DataContracts;
 using IcVibrations.DataContracts.Beam;
 using IcVibrationsTest.Validators.BeamRequest.TestDatas;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using Xunit;
 
 namespace IcVibrationsTest.Validators.BeamRequest
 {
-    public class CircularBeamRequestValidatorTest
+    public class CircularBeamRequestValidatorTest : CircularBeamRequestValidator
     {
-        private readonly CircularBeamRequestValidator _validator;
-
-        public CircularBeamRequestValidatorTest()
-        {
-            this._validator = new CircularBeamRequestValidator();
-        }
-
         [Theory]
         [ClassData(typeof(ValidateRetangularInputData))]
         public void ValidateShapeInput_Should_AddError_When_ReceiveZero_And_DoNothing_Otherwise(double diameter, double thickness, int errorCount)
         {
             // Arrange
-            OperationResponseBase response = new OperationResponseBase();
+            OperationResponseBase responseBase = new OperationResponseBase();
             CircularBeamRequestData requestData = new CircularBeamRequestData
             {
                 Diameter = diameter,
                 Thickness = thickness
             };
 
-            //http://anthonygiretti.com/2018/08/26/how-to-unit-test-private-methods-in-net-core-applications-even-if-its-bad/
-
             // Act
+            this.ValidateShapeInput(requestData, responseBase);
 
             // Assert
+            responseBase.Errors.Count
+                .Should()
+                .Be(errorCount);
         }
     }
 }
--- IcVibrations/IcVibrationsTest/Validators/BeamRequest/TestDatas/ValidateRetangularInputData.cs
+++ IcVibrations/IcVibrationsTest/Validators/BeamRequest/TestDatas/ValidateRetangularInputData.cs
@@ -8,12 +8,12 @@ namespace IcVibrationsTest.Validators.BeamRequest.TestDatas
         public ValidateRetangularInputData()
         {
             Add(0, 0, 1);
-            Add(double.MinValue, double.MinValue, 1);
             Add(double.MinValue, double.MaxValue, 2);
-            Add(-0.002, -0.0009, 1);
-            Add(-0.002, -0.002, 2);
+            Add(-0.002, -0.0009, 2);
+            Add(double.MaxValue, double.MaxValue, 1);
+            Add(double.MinValue, double.MinValue, 1);
+            Add(-0.002, -0.002, 1);
             Add(double.MaxValue, double.MinValue, 0);
-            Add(double.MaxValue, double.MaxValue, 0);
             Add(0.002, 0.001, 0);
         }
     }
