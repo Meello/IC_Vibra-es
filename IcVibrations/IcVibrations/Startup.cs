388a9746554c911d6d2a43b8f63c50fd903bdd74
Meello <bruno.silveira@stone.com.br>
2020-02-14 12:19:43 -0300

    Updated .Net Core 2.1 to 3.1 and .Net Standart 2.0 to 2.1.

--- IcVibrations/IcVibrations/Startup.cs
+++ IcVibrations/IcVibrations/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using IcVibrations.Calculator.GeometricProperties;
 using IcVibrations.Calculator.MainMatrixes;
-using IcVibrations.Common.Profiles;
 using IcVibrations.Core.Calculator.ArrayOperations;
 using IcVibrations.Core.Calculator.MainMatrixes.Beam.Circular;
 using IcVibrations.Core.Calculator.MainMatrixes.Beam.Rectangular;
@@ -10,10 +9,7 @@ using IcVibrations.Core.Calculator.MainMatrixes.BeamWithPiezoelectric.Rectangula
 using IcVibrations.Core.Mapper;
 using IcVibrations.Core.Mapper.Profiles.Circular;
 using IcVibrations.Core.Mapper.Profiles.Rectangular;
-using IcVibrations.Core.Models.BeamWithDynamicVibrationAbsorber;
-using IcVibrations.Core.Models.Piezoelectric;
 using IcVibrations.Core.NewmarkNumericalIntegration;
-using IcVibrations.Core.Operations;
 using IcVibrations.Core.Operations.Beam.Circular;
 using IcVibrations.Core.Operations.Beam.Rectangular;
 using IcVibrations.Core.Operations.BeamWithDva.Circular;
@@ -22,14 +18,13 @@ using IcVibrations.Core.Operations.BeamWithPiezoelectric.Circular;
 using IcVibrations.Core.Operations.BeamWithPiezoelectric.Rectangular;
 using IcVibrations.Core.Validators.Profiles.Circular;
 using IcVibrations.Core.Validators.Profiles.Rectangular;
-using IcVibrations.DataContracts.CalculateVibration.BeamWithDynamicVibrationAbsorber;
-using IcVibrations.DataContracts.CalculateVibration.BeamWithPiezoelectric;
 using IcVibrations.Methods.AuxiliarOperations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 namespace IcVibrations
@@ -85,7 +80,7 @@ namespace IcVibrations
             services.AddScoped<IRectangularProfileValidator, RectangularProfileValidator>();
             services.AddScoped<ICircularProfileValidator, CircularProfileValidator>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.AddSwaggerGen(c =>
             {
@@ -93,16 +88,11 @@ namespace IcVibrations
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
             }
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
@@ -115,7 +105,39 @@ namespace IcVibrations
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "IC Vibration V1");
             });
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
+
+        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //    }
+        //    else
+        //    {
+        //        app.UseHsts();
+        //    }
+
+        //    // Enable middleware to serve generated Swagger as a JSON endpoint.
+        //    app.UseSwagger();
+
+        //    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+        //    // specifying the Swagger JSON endpoint.
+        //    app.UseSwaggerUI(c =>
+        //    {
+        //        c.SwaggerEndpoint("/swagger/v1/swagger.json", "IC Vibration V1");
+        //    });
+
+        //    app.UseMvc();
+        //}
     }
 }
