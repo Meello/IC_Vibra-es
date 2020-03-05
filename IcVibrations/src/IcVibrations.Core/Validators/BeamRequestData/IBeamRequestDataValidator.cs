854493dee5509524020159b52d1fda4b9c451cfd
Meello <bruno.silveira@stone.com.br>
2020-03-05 17:02:19 -0300

    Created validator to BeamRequestData.

---
+++ IcVibrations/src/IcVibrations.Core/Validators/BeamRequestData/IBeamRequestDataValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IcVibrations.Core.Validators.BeamRequestData
+{
+    public interface IBeamRequestDataValidator
+    {
+    }
+}
